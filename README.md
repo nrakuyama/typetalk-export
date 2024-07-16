42704b2fd16657141c8cd042f0a3d26073ccc4cc
Noriaki Rakuyama <noriaki.rakuyama@kitalive.co.jp>
2024-07-16 15:05:40 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Typetalk ファイルエクスポート
+
+## つかいかた
+
+### Typetalk ボットの作成
+
+https://www.typetalk.com/ja/blog/post-a-message-to-typetalk-using-a-bot/#i  
+ファイルをエクスポートしたいトピックに対して、上記URLを参考にボットを作成する  
+APIスコープは「topic.read」のみとする  
+Typetalk Tokenをメモする
+
+### スクリプト実行
+
+* このリポジトリをcloneする
+* 初期設定を行う
+* コマンドを実行する
---
+++ export-files.js
@@ -0,0 +1,46 @@
+import fs from 'fs';
+import yargs from 'yargs'
+import { hideBin } from 'yargs/helpers'
+import { parse } from 'csv-parse/sync';
+import got from 'got';
+
+const main = async () => {
+  const args = yargs(hideBin(process.argv))
+    .options({
+      csvfile: {
+        type: 'string',
+        describe: 'path to attachments csv file',
+        demandOption: true,
+        alias: 'c'
+      },
+      token: {
+        type: 'string',
+        describe: 'token for Typetalk bot',
+        demandOption: true,
+        alias: 't'
+      },
+      output: {
+        type: 'string',
+        describe: 'directory to export attachment files',
+        demandOption: false,
+        default: '.',
+        alias: 'o'
+      }
+    })
+    .parseSync();
+
+  const { csvfile, output }  = args;
+  const records = parse(fs.readFileSync(csvfile), { columns: true });
+  fs.mkdirSync(output, { recursive: true });
+  const headers = { 'X-TYPETALK-TOKEN': args.token };
+
+  for (const r of records) {
+    const url = r['添付ファイル URL (API)'];
+    const title = r['添付ファイル名'];
+    const rawBody = (await got.get(url, { headers })).rawBody;
+    fs.writeFileSync(`${args.output}/${title}`, rawBody);
+    console.log(title);
+  }
+}
+
+await main();
